a087efd9bd562b123c4612c1c22423ec21440928
Derek Hypolite <67697826+DerekHypo@users.noreply.github.com>
2023-01-06 04:28:22 -0600

    Add files via upload

--- MechaCar_Statistical_Analysis/MechaCarChallenge.R
+++ MechaCar_Statistical_Analysis/MechaCarChallenge.R
@@ -12,3 +12,29 @@ mpg_summ <- summary(mpg_model)
 
 # Display the summary.
 mpg_summ
+
+# Import Suspension_Coil CSV and read it in as a DF
+susp_coil <- read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFactors = F)
+
+# Total summary of all the coils.
+total_summary <- susp_coil %>%
+  summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = "keep")
+
+# Get the lot summary of the coils.
+lot_summary <- susp_coil %>% group_by(Manufacturing_Lot) %>%
+  summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = "keep")
+
+# Check lot_summary
+lot_summary
+
+#T-Test for all Suspension Coils.
+t.test(susp_coil$PSI, mu=1500)
+
+#T-Test for Lot 1.
+t.test(subset(susp_coil, Manufacturing_Lot == 'Lot1')$PSI, mu = 1500)
+
+#T-Test for Lot 2.
+t.test(subset(susp_coil, Manufacturing_Lot == 'Lot2')$PSI, mu = 1500)
+
+#T-Test for Lot 3.
+t.test(subset(susp_coil, Manufacturing_Lot == 'Lot3')$PSI, mu = 1500)
